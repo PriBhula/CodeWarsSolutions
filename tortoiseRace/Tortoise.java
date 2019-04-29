1c4308d168a06a7ef478bbd10fd36eb68485c192
Priyanka <pridotbhula@gmail.com>
2019-04-29 16:44:05 +1200

    tortoise race

---
+++ tortoiseRace/Tortoise.java
@@ -0,0 +1,15 @@
+
+public class Tortoise {
+
+	 public static int[] race(int v1, int v2, int g) {
+		 
+	      if (v1 >= v2){
+	        return null;
+	      }
+	      
+	      int seconds = (g * 3600) / (v2 - v1);
+	      int[] ans = new int[]{seconds / 3600, (seconds % 3600) / 60, seconds % 60};
+	      return ans;
+	    
+	    }
+}
